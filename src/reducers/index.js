513ca9d4844fc7fb6234ecf657a2a0030836f75c
Rajesh Mohanty <mohantyrajesh1984@gmail.com>
2021-10-27 16:29:01 +0530

    add edit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,5 @@
 const tasks = (state = { tasks: [] }, action) => {
   switch (action.type) {
-    case "CREATE_TASK": {
-      return { tasks: state.tasks.concat(action.payload) };
-    }
-    case "EDIT_TASK": {
-      const { payload } = action;
-      return {
-        tasks: state.tasks.map((task) => {
-          if (task.id === payload.id) {
-            return Object.assign({}, task, payload.params);
-          }
-          return task;
-        }),
-      };
-    }
     case "FETCH_TASKS_SUCCEDED": {
       return {
         tasks: action.payload.tasks,
@@ -22,6 +8,15 @@ const tasks = (state = { tasks: [] }, action) => {
     case "CREATE_TASK_SUCCEEDED": {
       return {
         tasks: state.tasks.concat(action.payload.task),
+      };
+    }
+    case "EDIT_TASK_SUCCEEDED": {
+      const { payload } = action;
+      return {
+        tasks: state.tasks.map((task) => {
+          if (task.id === payload.task.id) return payload.task;
+          return task;
+        }),
       };
     }
     default: {
